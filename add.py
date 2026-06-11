65acd35e728a2d77fccb24d7ed51933fc0a23c73
Vinay <vinay00mahto@gmail.com>
2026-06-12 03:59:16 +0530

    add

--- add.py
+++ add.py
@@ -1,9 +1,10 @@
 a = 10
 b = 2
+print(a+b)
 
-print(a>b)
-print(a<b)
-print(a>=b)
-print(a<=b)
-print(a==b)
-print(a != b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+# print(a==b)
+# print(a != b)
